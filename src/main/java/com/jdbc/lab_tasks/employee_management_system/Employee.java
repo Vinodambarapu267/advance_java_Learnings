3efdbff160562939b77c6db9880e5f4ec7d7fccd
Vinodambarapu267 <ambarapuvinod@gmail.com>
2025-10-13 21:50:20 +0530

    feat: implemented the Employee class

---
+++ src/main/java/com/jdbc/lab_tasks/employee_management_system/Employee.java
@@ -0,0 +1,40 @@
+package com.jdbc.lab_tasks.employee_management_system;
+
+
+
+public class Employee { private int id;
+    private  String name;
+    private  double salary;
+    private String department;
+
+    public Employee(String name, double salary, String department) {
+
+        this.name = name;
+        this.salary = salary;
+        this.department = department;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+}
